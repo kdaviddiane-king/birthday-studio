edb02876f4363f7fcc86a9bd3383ec9fa07541e7
kdaviddiane-king <kdaviddiane@gmail.com>
2026-03-18 14:27:34 +0000

    Rename supabase variable to sbClient

--- birthday-studio/src/supabase.js
+++ birthday-studio/src/supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = 'https://hialqukjbguzfueionhx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpYWxxdWtqYmd1emZ1ZWlvbmh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MjYwMjYsImV4cCI6MjA4OTQwMjAyNn0.wH4OiBaDFoxdseMxFoWYjjmP7CVkTfXCYKC6TgN8i1Q';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const sbClient  = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-window.sb = supabase;
+window.sb = sbClient;
