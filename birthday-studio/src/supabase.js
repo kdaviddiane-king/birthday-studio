c118dbb5201dbdf8174e72cd8e88ee1034b0cb13
kdaviddiane-king <kdaviddiane@gmail.com>
2026-03-18 13:43:13 +0000

    Update supabase.js

--- birthday-studio/src/supabase.js
+++ birthday-studio/src/supabase.js
@@ -1,9 +1,6 @@
+const SUPABASE_URL = 'sb_publishable_CO5jUFgm2pDejQt9Cfe2qw_fMYmqWir';
+const SUPABASE_ANON_KEY = 'sb_secret_hVd8UAY4GQclrIh4LtJSNg_8a5Eyfaf';
 
-const SUPABASE_URL = 'sb_publishable_CO5jUFgm2pDejQt9Cfe2qw_fMYmqWir';      // ex: https://xxxx.supabase.co
-const SUPABASE_ANON_KEY = 'sb_secret_hVd8UAY4GQclrIh4LtJSNg_8a5Eyfaf'; // clé publique (anon)
-
-// Initialisation du client Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Export global
 window.sb = supabase;
