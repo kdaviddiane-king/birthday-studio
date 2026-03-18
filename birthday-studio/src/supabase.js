77bd7094b7ea1ad68a1f34f9a6f709d2957c7973
kdaviddiane-king <kdaviddiane@gmail.com>
2026-03-18 14:03:24 +0000

    Update Supabase URL and anon key

--- birthday-studio/src/supabase.js
+++ birthday-studio/src/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'sb_publishable_CO5jUFgm2pDejQt9Cfe2qw_fMYmqWir';
-const SUPABASE_ANON_KEY = 'sb_secret_hVd8UAY4GQclrIh4LtJSNg_8a5Eyfaf';
+const SUPABASE_URL = 'https://hialqukjbguzfueionhx.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpYWxxdWtqYmd1emZ1ZWlvbmh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MjYwMjYsImV4cCI6MjA4OTQwMjAyNn0.wH4OiBaDFoxdseMxFoWYjjmP7CVkTfXCYKC6TgN8i1Q';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
